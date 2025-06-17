69aa0f8498eba6abe4cf3fa996ccb0f1275cd0ec
Mannypulator <emeritus93@gmail.com>
2025-06-17 10:40:33 +0100

    push

--- Tracklio/Program.cs
+++ Tracklio/Program.cs
@@ -17,11 +17,10 @@ builder.Services.RegisterInfrastructureServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
